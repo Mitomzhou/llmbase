288ca7465b1603923c4c74da57da4c485131d247
Mitomzhou <zhouyong_monky@163.com>
2024-03-27 17:11:48 +0800

    transformer模型实现

--- translator/model.py
+++ translator/model.py
@@ -1,10 +1,16 @@
+"""
+Transfomer参考https://zhuanlan.zhihu.com/p/144825330
+"""
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import math
+import copy
+import numpy as np
 
-DEVICE = ""
+DEVICE = torch.device("cuda")
 
 class Embeddings(nn.Module):
     def __init__(self, vocab, d_model):
@@ -17,7 +23,11 @@ class Embeddings(nn.Module):
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout, max_len=5000):
+    """
+        使用多个周期不同的周期函数组成的多维度编码和递增序列编码其实是可以等价的
+        参考：https://blog.csdn.net/m0_37605642/article/details/132866365
+    """
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
 
@@ -40,5 +50,290 @@ class PositionalEncoding(nn.Module):
 
 
 def attention(query, key, value, mask=None, dropout=None):
-    dk = query.size(-1)
+    # 将query矩阵的最后一个维度值作为d_k
+    d_k = query.size(-1)
+
+    # 将key的最后两个维度互换(转置)，才能与query矩阵相乘，乘完了还要除以d_k开根号
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+
+    # 如果存在要进行mask的内容，则将那些为0的部分替换成一个很大的负数
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e9)
+
+    # 将mask后的attention矩阵按照最后一个维度进行softmax
+    p_attn = F.softmax(scores, dim=-1)
+
+    # 如果dropout参数设置为非空，则进行dropout操作
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    # 最后返回注意力矩阵跟value的乘积，以及注意力矩阵
+    return torch.matmul(p_attn, value), p_attn
+
+
+class MultiHeadedAttention(nn.Module):
+    """
+    # 帮助理解代码
+    nbatch = 64
+    seq_len = 50
+    h = 8
+    dmodel = 512
+    dk = 512 // 8
+    # linear复制4份,前3份用来做QKV的Wx，最后一份用来处理attn的线性变换
+    linears = clones(nn.Linear(512,512), 4)
+
+    query = torch.randn((64,50,512))
+    key = torch.randn((64,50,512))
+    value = torch.randn((64,50,512))
+    # 构建 Q K V
+    query, key, value =  [l(x).view(64, -1, 8, dk).transpose(1,2) for l, x in zip(linears, (query, key, value))]
+
+    print("q k v:",query.shape)
+    # K转置
+    key_t = key.transpose(-2,-1)
+    print("key_t:",key_t.shape)
+    # Q * Kt
+    scores = torch.matmul(query, key_t)
+    print("scores:",scores.shape)
+    # QktV
+    attn = torch.matmul(scores, value)
+    print("attn:",attn.shape)
+    # 输出
+    q k v: torch.Size([64, 8, 50, 64])
+    key_t: torch.Size([64, 8, 64, 50])
+    scores: torch.Size([64, 8, 50, 50])
+    attn: torch.Size([64, 8, 50, 64])
+    合并concat后经过linear
+    attn: torch.Size([64, 50, 8 * 64])
+    """
+    def __init__(self, h, d_model, dropout=0.1):
+        super(MultiHeadedAttention, self).__init__()
+        # 保证可以整除
+        assert d_model % h == 0
+        # 得到一个head的attention表示维度
+        self.d_k = d_model // h
+        # head数量
+        self.h = h
+        # 定义4个全连接函数，供后续作为WQ，WK，WV矩阵和最后h个多头注意力矩阵concat之后进行变换的矩阵
+        self.linears = clones(nn.Linear(d_model, d_model), 4)
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, query, key, value, mask=None):
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+        # query的第一个维度值为batch size
+        nbatches = query.size(0)
+        # 将embedding层乘以WQ，WK，WV矩阵(均为全连接)
+        # 并将结果拆成h块，然后将第二个和第三个维度值互换(具体过程见上述解析)
+        query, key, value = [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+                             for l, x in zip(self.linears, (query, key, value))]
+        # 调用上述定义的attention函数计算得到h个注意力矩阵跟value的乘积，以及注意力矩阵
+        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout)
+        # 将h个多头注意力矩阵concat起来（注意要先把h变回到第三维的位置）
+        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
+        # 使用self.linears中构造的最后一个全连接函数来存放变换后的矩阵进行返回
+        return self.linears[-1](x)
+
+
+def clones(module, N):
+    """克隆模型块，克隆的模型块参数不共享"""
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        # 初始化α为全1, 而β为全0
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        # 平滑项
+        self.eps = eps
+
+    def forward(self, x):
+        # 按最后一个维度计算均值和方差
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+
+        # 返回Layer Norm的结果
+        return self.a_2 * (x - mean) / torch.sqrt(std ** 2 + self.eps) + self.b_2
+
+
+class SublayerConnection(nn.Module):
+    """
+    SublayerConnection的作用就是把Multi-Head Attention和Feed Forward层连在一起
+    只不过每一层输出之后都要先做Layer Norm再残差连接
+    sublayer是lamda函数
+    """
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        # 返回Layer Norm和残差连接后结果
+        return x + self.dropout(sublayer(self.norm(x)))
+
+
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+
+
+class Encoder(nn.Module):
+    # layer = EncoderLayer
+    # N = 6
+    def __init__(self, layer, N):
+        super(Encoder, self).__init__()
+        # 复制N个encoder layer
+        self.layers = clones(layer, N)
+        # Layer Norm
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, mask):
+        """
+        使用循环连续encode N次(这里为6次)
+        这里的Eecoderlayer会接收一个对于输入的attention mask处理
+        """
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, size, self_attn, feed_forward, dropout=0.1):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        # SublayerConnection的作用就是把multi和ffn连在一起
+        # 只不过每一层输出之后都要先做Layer Norm再残差连接
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        # d_model
+        self.size = size
+
+    def forward(self, x, mask):
+        # 将embedding层进行Multi head Attention
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+        # 注意到attn得到的结果x直接作为了下一层的输入
+        return self.sublayer[1](x, self.feed_forward)
+
+"""
+解码过程
+输入：Encoder 的输出 & 对应 i−1 位置 Decoder 的输出。所以中间的 Attention 不是 Self-Attention ，它的 K，V 来自 Encoder ，Q来自上一位置 Decoder 的输出
+输出：对应 i 位置的输出词的概率分布
+解码：这里要特别注意一下，编码可以并行计算，一次性全部encoding出来，但解码不是一次把所有序列解出来的，而是像rnn一样一个一个解出来的，因为要用上一个位置的输入当作 Attention 的 query
+"""
+
+class Decoder(nn.Module):
+    def __init__(self, layer, N):
+        super(Decoder, self).__init__()
+        # 复制N个encoder layer
+        self.layers = clones(layer, N)
+        # Layer Norm
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        """
+        使用循环连续decode N次(这里为6次)
+        这里的Decoderlayer会接收一个对于输入的attention mask处理
+        和一个对输出的attention mask + subsequent mask处理
+        """
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
+        return self.norm(x)
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout=0.1):
+        super(DecoderLayer, self).__init__()
+        self.size = size
+        # Self-Attention
+        self.self_attn = self_attn
+        # 与Encoder传入的Context进行Attention
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        # 用m来存放encoder的最终hidden表示结果
+        m = memory
+
+        # Self-Attention：注意self-attention的q，k和v均为decoder hidden
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        # Context-Attention：注意context-attention的q为decoder hidden，而k和v为encoder hidden
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
+
+def subsequent_mask(size):
+    "Mask out subsequent positions."
+    # 设定subsequent_mask矩阵的shape
+    attn_shape = (1, size, size)
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    return torch.from_numpy(subsequent_mask) == 0
+
+
+class Transformer(nn.Module):
+    def __init__(self, encoder, decoder, src_embed, tgt_embed, generator):
+        super(Transformer, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.generator = generator
+
+    def encode(self, src, src_mask):
+        return self.encoder(self.src_embed(src), src_mask)
+
+    def decode(self, memory, src_mask, tgt, tgt_mask):
+        return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
+
+    def forward(self, src, tgt, src_mask, tgt_mask):
+        # encoder的结果作为decoder的memory参数传入，进行decode
+        return self.decode(self.encode(src, src_mask), src_mask, tgt, tgt_mask)
+
+
+class Generator(nn.Module):
+    # vocab: tgt_vocab
+    def __init__(self, d_model, vocab):
+        super(Generator, self).__init__()
+        # decode后的结果，先进入一个全连接层变为词典大小的向量
+        self.proj = nn.Linear(d_model, vocab)
+
+    def forward(self, x):
+        # 然后再进行log_softmax操作(在softmax结果上再做多一次log运算)
+        return F.log_softmax(self.proj(x), dim=-1)
+
+
+
+import copy
+h = 8
+N = 6
+d_model = 512
+d_ff = 2048
+dropout = 0.1
+
+src_vocab_size = 32000
+tgt_vocab_size = 32000
+
+c = copy.deepcopy
+attn = MultiHeadedAttention(h, d_model).to(DEVICE)
+ff = PositionwiseFeedForward(d_model, d_ff).to(DEVICE)
+position = PositionalEncoding(d_model).to(DEVICE)
+
+model = Transformer(
+    Encoder(EncoderLayer(d_model, c(attn), c(ff)).to(DEVICE), N).to(DEVICE),
+    Decoder(DecoderLayer(d_model, c(attn), c(attn), c(attn)).to(DEVICE), N).to(DEVICE),
+    nn.Sequential(Embeddings(d_model, src_vocab_size).to(DEVICE), c(position)),
+    nn.Sequential(Embeddings(d_model, tgt_vocab_size).to(DEVICE), c(position)),
+    Generator(d_model, tgt_vocab_size).to(DEVICE)
+)
+
+print(model)
+from torchinfo import summary
+summary(model)
 
